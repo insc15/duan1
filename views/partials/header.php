7995c5d88f6744d9d1ab7782fc3552c933b0e9fc
Scr18 <nguyenquanghuyx8@gmail.com>
2022-11-18 16:50:47 +0700

    up

--- views/partials/header.php
+++ views/partials/header.php
@@ -4,19 +4,19 @@
         <nav class="font-semibold ml-auto flex items-center">
             <a class="text-primary relative md:block hidden after:content-[''] after:w-2/3 after:bg-primary after:h-[2px] after:left-0 after:-bottom-1 after:absolute mx-5" href="<?php get_home_url() ?>">Home</a>
             <div>
-            <a class="hover:text-primary md:block hidden duration-150 relative after:content-[''] hover:after:w-2/3 after:w-0 after:duration-150 after:bg-primary after:h-[2px] after:left-0 after:-bottom-1 after:absolute mx-5 peer" id="dropdownDefault" data-dropdown-toggle="dropdown" href="#">Shop</a>
-            <div class="hidden peer-hover:flex hover:flex
-         w-[200px]
-         flex-col bg-white drop-shadow-lg">
-            <a class="px-5 py-3 hover:bg-gray-200" href="#">About Us</a>
-            <a class="px-5 py-3 hover:bg-gray-200" href="#">Contact Us</a>
-            <a class="px-5 py-3 hover:bg-gray-200" href="#">Privacy Policy</a>
+            <a class="hover:text-primary hidden peer relative md:block hidden duration-150 relative after:content-[''] hover:after:w-2/3 after:w-0 after:duration-150 after:bg-primary after:h-[2px] after:left-0 after:-bottom-1 after:absolute mx-5 " id="dropdownDefault" data-dropdown-toggle="dropdown" href="#"><span class="">Shop</span><span class="material-symbols-rounded">expand_more</span></a>
+            <!-- dropdown-menu -->
+            <div class="hidden absolute pt-5 peer-hover:flex hover:flex w-[200px] flex-col bg-white drop-shadow-lg ">
+            <a class="px-5 py-3 hover:bg-gray-200" href="<?php get_home_url()?>">danh muc ?</a>
+            <a class="px-5 py-3 hover:bg-gray-200" href="<?php get_home_url()?>">danh muc ?</a>
+            <a class="px-5 py-3 hover:bg-gray-200" href="<?php get_home_url()?>">danh muc ?</a>
         </div>
         </div>
             <a class="hover:text-primary md:block hidden duration-150 relative after:content-[''] hover:after:w-2/3 after:w-0 after:duration-150 after:bg-primary after:h-[2px] after:left-0 after:-bottom-1 after:absolute mx-5" href="<?php get_home_url() ?>">Blog</a>
             <a class="hover:text-primary md:block hidden duration-150 relative after:content-[''] hover:after:w-2/3 after:w-0 after:duration-150 after:bg-primary after:h-[2px] after:left-0 after:-bottom-1 after:absolute mx-5" href="<?php get_home_url() ?>">About</a>
             <a class="hover:text-primary md:block hidden duration-150 relative after:content-[''] hover:after:w-2/3 after:w-0 after:duration-150 after:bg-primary after:h-[2px] after:left-0 after:-bottom-1 after:absolute mx-5" href="<?php get_home_url() ?>">Contact</a>
             <a class="hover:text-primary duration-150 relative after:content-[''] hover:after:w-full after:w-0 after:duration-150 after:bg-primary after:h-[2px] after:left-0 after:-bottom-1 after:absolute mx-5" href="<?php get_home_url() ?>"><span class="material-symbols-rounded icon-outline">shopping_cart</span></a>
+            
         </nav>
         <div class="md:hidden"><span class="material-symbols-rounded icon-outline">menu</span></div>
     </div>
