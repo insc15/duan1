95e84a1678b1fa832f863314a5c6350e3510f2bb
viet <viet20000333@gmail.com>
2022-11-18 08:56:57 +0700

    ttt

--- views/pages/shop-detail.php
+++ views/pages/shop-detail.php
@@ -1,3 +1,48 @@
-<section>
-    detail
+<section class="flex">
+
+  <div class=" w-5/12 px-5 lg:px-20 mt-10  ">
+    
+    <div class="">
+      <img src="https://templateprj.vercel.app/img/products/f1.jpg" />
+      
+    </div>
+    <div class="flex">
+      <div class="w-4/12  mt-2  ">
+      <img src="https://templateprj.vercel.app/img/products/f1.jpg"  alt="">
+    </div>
+    <div class="w-4/12  mt-2  ml-1 ">
+      <img src="https://templateprj.vercel.app/img/products/f1.jpg"  alt="">
+    </div>
+    <div class="w-4/12  mt-2 ml-1 ">
+      <img src="https://templateprj.vercel.app/img/products/f1.jpg"  alt="">
+    </div>
+    <div class="w-4/12  mt-2 ml-1 ">
+      <img src="https://templateprj.vercel.app/img/products/f1.jpg"  alt="">
+    </div>
+    <div class="w-4/12  mt-2 ml-1 ">
+      <img src="https://templateprj.vercel.app/img/products/f1.jpg"  alt="">
+    </div>
+  </div>
+ 
+</div>
+<div class="w-7/12 mt-10 ">
+  <div class="font-bold">
+     <p>Home / T-shirt</p>
+  </div>
+  <div class=" py-5 font-bold text-2xl">
+    Men's Fashion T Shirt
+    <p class="py-5 text-3xl text-primary flex  ">
+      $139.00 
+      <span class="ml-3 line-through text-gray-400"> 1.000.000</span>
+    </p>
+  </div>
+  <div class=" float-none">
+    <p>The Gildan Cotton T-Shirt it made from a substantial 6.0 oz. per sq. yd. fabric contructer from 100% cotton, this classic fit preshrunk jersey knit provides unmatched comfort with each wear. Featuring a taped neck and shoulder, and a seamless double-needle collar, and available in a range of colors, it offers it all in the ultimate head-turning package</p>
+  </div>
+ 
+
+</div>
+
+    
+
 </section>
