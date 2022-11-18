a98ae864aec2a429667268b29505401f1966f13a
viet <viet20000333@gmail.com>
2022-11-18 23:46:31 +0700

    11182022

--- views/pages/product.php
+++ views/pages/product.php
@@ -59,11 +59,7 @@
           <div
             class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
           >
-            <svg
-              class="fill-current h-4 w-4"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
+           
               <path
                 d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
               />
@@ -106,7 +102,7 @@
     <label
       for="large-input"
       class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-      >Comment</label
+      ></label
     >
 
     <input
