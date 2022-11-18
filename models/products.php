d5e8ac415b5a91a63658ab44431c2ad9ec3ec36b
Minh Hieu <ngohieu20031@gmail.com>
2022-11-18 08:57:45 +0700

    up

--- models/products.php
+++ models/products.php
@@ -1,8 +1,33 @@
 <?php
 require_once('models/config.php');
-function get_list_products()
+
+function get_list_products($meta_query = null)
 {
     $sql = "SELECT * FROM product";
     $product = getData($sql, FETCH_ALL);
+    $formatted_product = format_product($product);
+    return $formatted_product;
+}
+
+// chỗ này để format các trường thông tin
+function format_product($product){
+    switch (gettype($product)) {
+        case 'integer':
+            $product = formatter($product);
+            break;
+        case 'array':
+            $product = array_map(function($item){
+                return formatter($item);
+            },$product);
+        default:
+            # code...
+            break;
+    }
+
     return $product;
+}
+
+function formatter($item){
+    $item['featured'] = boolval($item['featured']);
+    return $item;
 }
