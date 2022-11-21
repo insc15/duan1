96d02c55a1bb34ef9ff5ee7d679daedb1fd41048
viet <viet20000333@gmail.com>
2022-11-21 08:27:39 +0700

    11212022

--- views/pages/product.php
+++ views/pages/product.php
@@ -6,36 +6,22 @@
       </div>
       <div class="flex">
         <div class="w-4/12 mt-2">
-          <img
-            src="https://templateprj.vercel.app/img/products/f1.jpg"
-            alt=""
-          />
+          <img src="https://templateprj.vercel.app/img/products/f1.jpg" alt="" />
         </div>
         <div class="w-4/12 mt-2 ml-1">
-          <img
-            src="https://templateprj.vercel.app/img/products/f1.jpg"
-            alt=""
-          />
+          <img src="https://templateprj.vercel.app/img/products/f1.jpg" alt="" />
         </div>
         <div class="w-4/12 mt-2 ml-1">
-          <img
-            src="https://templateprj.vercel.app/img/products/f1.jpg"
-            alt=""
-          />
+          <img src="https://templateprj.vercel.app/img/products/f1.jpg" alt="" />
         </div>
         <div class="w-4/12 mt-2 ml-1">
-          <img
-            src="https://templateprj.vercel.app/img/products/f1.jpg"
-            alt=""
-          />
+          <img src="https://templateprj.vercel.app/img/products/f1.jpg" alt="" />
         </div>
         <div class="w-4/12 mt-2 ml-1">
-          <img
-            src="https://templateprj.vercel.app/img/products/f1.jpg"
-            alt=""
-          />
+          <img src="https://templateprj.vercel.app/img/products/f1.jpg" alt="" />
         </div>
       </div>
+     
     </div>
 
     <div class="w-7/12 mt-10">
@@ -49,20 +35,10 @@
           <span class="ml-3 line-through text-gray-400"> 1.000.000</span>
         </p>
         <div class="inline-block relative w-[2rem]">
-          <select
-            class="block appearance-none w-[2rem] bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline text-center text-xs"
-          >
-            <option id="">S</option>
-            <option id="">L</option>
-            <option id="">XL</option>
-          </select>
-          <div
-            class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
-          >
-           
-              <path
-                d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-              />
+
+          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+
+            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
             </svg>
           </div>
         </div>
@@ -77,9 +53,60 @@
           head-turning package
         </p>
       </div>
+     <div class="flex">
+      
+      <div class="flex flex-wrap">
+        <p class="font-bold mt-2">color:</p>
+        <div class="flex items-center mr-4">
+            <input id="red-radio" type="radio" value="" name="colored-radio" class="ml-2 w-10 h-10 accent-black  rounded-full text-black "  >
+            <label for="red-radio" class="ml-2 text-sm font-medium  dark:text-gray-300"></label>
+        </div>
+        <div class="flex items-center mr-4">
+            <input id="green-radio" type="radio" value="" name="colored-radio" class="w-10 h-10 accent-white outline-slate-50 rounded-full">
+            <label for="green-radio" class="ml-2 text-sm font-medium dark:text-gray-300"></label>
+        </div>
+        <div class="flex items-center mr-4">
+            <input checked id="purple-radio" type="radio" value="" name="colored-radio" class="w-10 h-10 accent-red-500 rounded-full">
+            <label for="purple-radio" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"></label>
+        </div>
+        <div class="flex items-center mr-4">
+            <input id="teal-radio" type="radio" value="" name="colored-radio" class="w-10 h-10 accent-blue-700 rounded-full">
+            <label for="teal-radio" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"></label>
+        </div>
+        
+    </div>
+    
+    
+    
+    
+
+      </div>
+      <div class="inline-block relative w-25 mt-3 ">
+        
+        <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+          <option>size</option>
+          <option>XL</option>
+          <option>L</option>
+          <option>M</option>
+          <option>S</option>
+        </select>
+        
+      </div>
+      
+       
+     </div>
+     <div class="">
+      <input type="text" class="w-10" >
+     </div>
+    
+        
+
     </div>
   </div>
-  <hr class="mt-2" />
+  <div class=" w-5/12 px-5 lg:px-20 mt-10">
+    <h1 class="font-bold">Description</h1>
+  </div>
+  <hr class="mt-2 " />
   <div class="px-5 lg:px-20 mt-6">
     The Gildan Cotton T-Shirt it made from a substantial 6.0 oz. per sq. yd.
     fabric contructer from 100% cotton, this classic fit preshrunk jersey knit
@@ -97,18 +124,11 @@
     double-needle collar, and available in a range of colors, it offers it all
     in the ultimate head-turning package
   </div>
-  <hr />
+  <hr class="mt-4"/>
   <div class="mb-6 mt-10 flex">
-    <label
-      for="large-input"
-      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-      ></label
-    >
+    <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
 
-    <input
-      type="text"
-      id="large-input"
-      class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ml-5"
-    />
+    <input type="text" id="large-input"
+      class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ml-5" />
   </div>
 </section>
