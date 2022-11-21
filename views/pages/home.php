fb77e33d4f16c9bab1ee337d8ecbca298001ada6
Minh Hieu <ngohieu20031@gmail.com>
2022-11-21 22:53:20 +0700

    f

--- views/pages/home.php
+++ views/pages/home.php
@@ -18,9 +18,9 @@ $list_product= view_products();
         <p class="md:text-base text-sm">Summer Collection New Morden Design</p>
     </div>
     <div class="flex flex-wrap max-w-screen-xl -mx-1 -my-2 md:mx-auto px-4">
-        <?php foreach ($list_product as $value) : if ($value['featured']) : ?>
-            <div class="w-1/2 md:w-1/4 py-2 md:py-0 px-1 md:px-3">
-                <a href="<?php echo get_home_url()."/product?id=".$value['id'] ?>" class="block bg-white rounded-lg md:hover:shadow-xl md:shadow border-primary md:border md:p-3 ease-in-out duration-150">
+        <?php foreach (get_list_prfeatured() as $value) :?>
+            <div class="w-1/2 md:w-1/4 py-2 px-1 md:px-3">
+                <a href="<?php echo get_home_url()."/product?id=".$value['id'] ?>" class="grid h-full bg-white rounded-lg md:hover:shadow-xl md:shadow border-primary md:border md:p-3 ease-in-out duration-150">
                     <div class="rounded pt-[100%] overflow-hidden relative">
                         <img class="absolute top-0" src="<?php echo $value['featured_image'] ?>" alt=""/>
                     </div>
@@ -36,13 +36,13 @@ $list_product= view_products();
                         </div>
                         <p class="text-primary font-bold mt-1"><?php echo $value['formatted_price'] ?></p>
                     </div>
-                    <button class="bg-secondary hover:bg-primary rounded w-full py-2 justify-center text-white flex items-center leading-normal text-xs md:text-base">
+                    <button class="bg-secondary hover:bg-primary rounded w-full mt-auto py-2 justify-center text-white flex items-center leading-normal text-xs md:text-base">
                         <span class="material-symbols-rounded icon-outline hidden md:block">add_shopping_cart</span>
                         ADD TO CART
                     </button>
                 </a>
             </div>
-        <?php endif; endforeach; ?>
+        <?php endforeach; ?>
     </div>
     <a href="#" class="mt-8 w-fit mx-auto md:text-base text-sm block rounded border-2 border-primary py-3 px-4 font-semibold text-primary hover:bg-primary hover:text-white duration-150">Explore More</a>
 </section>
@@ -53,9 +53,9 @@ $list_product= view_products();
         <p class="md:text-base text-sm">Summer Collection New Morden Design</p>
     </div>
     <div class="flex flex-wrap max-w-screen-xl -mx-1 -my-2 md:mx-auto px-4">
-        <?php foreach ($list_product as $value) : if ($value['featured']) : ?>
-            <div class="w-1/2 md:w-1/4 py-2 md:py-0 px-1 md:px-3">
-                <a href="<?php echo get_home_url()."/product?id=".$value['id'] ?>" class="block bg-white rounded-lg md:hover:shadow-xl md:shadow border-primary md:border md:p-3 ease-in-out duration-150">
+        <?php foreach (get_list_products() as $value) :?>
+            <div class="w-1/2 md:w-1/4 py-2 px-1 md:px-3">
+                <a href="<?php echo get_home_url()."/product?id=".$value['id'] ?>" class="grid h-full bg-white rounded-lg md:hover:shadow-xl md:shadow border-primary md:border md:p-3 ease-in-out duration-150">
                     <div class="rounded pt-[100%] overflow-hidden relative">
                         <img class="absolute top-0" src="<?php echo $value['featured_image'] ?>" alt=""/>
                     </div>
@@ -71,13 +71,13 @@ $list_product= view_products();
                         </div>
                         <p class="text-primary font-bold mt-1"><?php echo $value['formatted_price'] ?></p>
                     </div>
-                    <button class="bg-secondary hover:bg-primary rounded w-full py-2 justify-center text-white flex items-center leading-normal text-xs md:text-base">
+                    <button class="bg-secondary hover:bg-primary rounded w-full mt-auto py-2 justify-center text-white flex items-center leading-normal text-xs md:text-base">
                         <span class="material-symbols-rounded icon-outline hidden md:block">add_shopping_cart</span>
                         ADD TO CART
                     </button>
                 </a>
             </div>
-        <?php endif; endforeach; ?>
+        <?php endforeach; ?>
     </div>
     <a href="#" class="mt-8 w-fit mx-auto md:text-base text-sm block rounded border-2 border-primary py-3 px-4 font-semibold text-primary hover:bg-primary hover:text-white duration-150">Explore More</a>
 </section>
