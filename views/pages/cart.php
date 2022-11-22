24fcc407be9844171378a6ee7ee3118e94a4e9b4
Scr18 <nguyenquanghuyx8@gmail.com>
2022-11-23 00:00:25 +0700

    up

--- views/pages/cart.php
+++ views/pages/cart.php
@@ -1,3 +1,37 @@
-<section>
-   <h1>cart</h1>
+
+<!-- banner -->
+<section class="bg-[url('../images/hero4.png')] bg-no-repeat bg-cover bg-center h-[70vh] md:h-96 lg:h-screen flex flex-col justify-center items-start"></section>
+
+<section class="pt-9 max-w-screen-xl mx-auto items-center">
+   
+      <div class="">
+         <h3 class="text-xl text-semibold">dit me thang hieu</h3>
+      </div>
+      <!-- main -->
+      <div class="flex justify-center">
+         <!-- left -->
+         <div class="w-8/12 pr-3">
+            <!-- products -->
+            <div class="flex justify-center  shadow border-primary m-1">
+                  <div class="w-2/12 overflow-hidden relative pt-[100%]">
+                     <img class="absolute top-0 w-24 " src="https://templateprj.vercel.app/img/products/f1.jpg" alt="">
+                  </div>
+                  <div class="w-5/12">
+                     zend_version
+                  </div>
+                  <div class="w-2/12">
+                     z
+                  </div>
+                  <div class="w-3/12">
+                    z 
+                  </div>
+                  
+            </div>
+            
+         </div>
+         <!-- right -->
+         <div class="w-4/12 pl-3">
+            b
+         </div>
+      </div>
 </section>
