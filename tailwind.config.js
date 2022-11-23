2b8b222c83acf07a5fc50ba046ffb4f1db5ecf10
Do Duy Long <103615878+long1308@users.noreply.github.com>
2022-11-24 04:03:15 +0700

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,20 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'primary': ['Spartan', 'sans-serif']
+        primary: ["Spartan", "sans-serif"],
       },
-      colors:{
-        primary: '#088178',
-        secondary: '#041E42',
+      colors: {
+        primary: "#088178",
+        secondary: "#041E42",
         // brown: '#52433f'
+      },
+      boxShadow: {
+        boxShadowInput: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
       },
       // boxShadow: {
       //   'default': '0 0 20px 20px rgba(0,0,0,0.02)',
       // }
     },
   },
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-  ],
-}
+  plugins: [require("@tailwindcss/line-clamp")],
+};
