d504a9c8f611247eb7931a7759be4a610e381251
Minh Hieu <ngohieu20031@gmail.com>
2022-11-24 23:22:56 +0700

    up

--- models/products.php
+++ models/products.php
@@ -13,22 +13,22 @@ function get_product($meta_query = null)
     return $formatted_product;
 }
 
-function get_one_product($id)
-{
-    $sql = "SELECT * FROM product where id = $id";
-    $product = getData($sql, FETCH_ONE);
-    $formatted_product = format_product($product);
-    return $formatted_product;
-}
-// kt hàng mới
-function get_list_prnew($day)
-{
-    // đổi lấy giây ngày hiện tại
-    $date_second =  time();
-    // đổi lấy giây ngày tạo sản phẩm
-    $sc_product = strtotime($day);
-    return strftime('%d', $date_second - $sc_product);
-}
+// function get_one_product($id)
+// {
+//     $sql = "SELECT * FROM product where id = $id";
+//     $product = getData($sql, FETCH_ONE);
+//     $formatted_product = format_product($product);
+//     return $formatted_product;
+// }
+// // kt hàng mới
+// function get_list_prnew($day)
+// {
+//     // đổi lấy giây ngày hiện tại
+//     $date_second =  time();
+//     // đổi lấy giây ngày tạo sản phẩm
+//     $sc_product = strtotime($day);
+//     return strftime('%d', $date_second - $sc_product);
+// }
 
 // chỗ này để format các trường thông tin
 function format_product($product)
@@ -50,6 +50,7 @@ function formatter($item)
     $item['formatted_price'] = number_format($item['price'], 0, '.', '.') . '&#8363;';
     $item['formatted_discount'] = number_format($item['discount'], 0, '.', '.') . '&#8363;';
     $item['list_image'] = explode(',', $item['list_image']);
+    
     $category_array = [];
     foreach (explode(',',$item['category']) as $key => $value) {
         $category = get_category(array('id' => $value));
@@ -70,6 +71,6 @@ function formatter($item)
         array_push($size_array, $size[0]);
     }
     $item['size'] = $size_array;
-
+    
     return $item;
 }
