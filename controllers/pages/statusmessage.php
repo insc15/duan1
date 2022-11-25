65d6800070ba0abe4d3ee4f5a8a17c761dd46bf8
Scr18 <nguyenquanghuyx8@gmail.com>
2022-11-25 11:12:59 +0700

    up ordersuccessful

--- controllers/pages/statusmessage.php
+++
@@ -1,3 +0,0 @@
-<?php
-    include('./views/pages/statusmessage.php');
-?>
--- views/pages/ordersuccessful.php
+++ views/pages/ordersuccessful.php
@@ -1,3 +1,14 @@
-<section>
-    
+<section class="w-full mx-auto h-screen">
+    <div class="w-[727px] shadow-lg rounded-sm border h-[381px] mx-auto flex flex-col items-center mt-10 font-[Montserrat]">
+        <svg class="mt-8"width="97" height="96" viewBox="0 0 97 96" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M48.5 96C75.0097 96 96.5 74.5097 96.5 48C96.5 21.4903 75.0097 0 48.5 0C21.9903 0 0.5 21.4903 0.5 48C0.5 74.5097 21.9903 96 48.5 96Z" fill="#25AE88"/>
+            <path d="M73.46 28.8L42.74 63.3601L23.54 48" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            <h2 class="text-4xl font-semibold mt-6">Your order is complete!</h2>
+            <p class="mt-4 text-xl font-normal">You will be receiving a confirmation email with order details.</p>
+            <p class="mt-4 text-sm font-normal">Order ID: CARA000000000123</p>
+            <a href="" class=" rounded-lg text-base font-normal flex mt-8 w-[211px] h-[40px] bg-[#041E42] text-white hover:bg-[white] hover:text-[#25AE88]  hover:border hover:border-[#25AE88]"><span class="material-symbols-rounded ml-6 text-base my-auto">arrow_back_ios</span><p class=" my-auto" >Continue shopping</p></a>
+    </div>
+        
+
 </section>
