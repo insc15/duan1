3618c2938cc175de80297d1ff1def620c434cecc
Scr18 <nguyenquanghuyx8@gmail.com>
2022-11-29 19:51:28 +0700

    admin register

--- views/pages/register.php
+++ views/pages/register.php
@@ -2,13 +2,19 @@
     <div class="w-[727px]">
         <h1 class="text-xl">Register</h1>
         <form action="" class="" method="post">
+            <input name="display_name" type="text"
+                class="px-3 py-2 focus:shadow-default focus:border-gray-500 focus:outline-none placeholder:text-sm w-full border border-solid border-gray-300 rounded-sm my-2 placeholder:text-center"
+                placeholder="Display Name">
             <input name="username" type="text"
                 class="px-3 py-2 focus:shadow-default focus:border-gray-500 focus:outline-none placeholder:text-sm w-full border border-solid border-gray-300 rounded-sm my-2 placeholder:text-center"
                 placeholder="Username">
             <input name="password" type="password"
                 class="px-3 py-2 focus:shadow-default focus:border-gray-500 focus:outline-none placeholder:text-sm w-full border border-solid border-gray-300 rounded-sm my-2"
                 placeholder="Password">
-            
+            <input name="email" type="password"
+                class="px-3 py-2 focus:shadow-default focus:border-gray-500 focus:outline-none placeholder:text-sm w-full border border-solid border-gray-300 rounded-sm my-2"
+                placeholder="Email">
+            <input type="file">
             <button class="w-full bg-primary text-primary py-2 px-3 mt-2" type="submit">register</button>
         </form>
     </div>
--- views/partials/admin/header.php
+++ views/partials/admin/header.php
@@ -1,3 +1,10 @@
-<section class="flex flex-col w-[300px] border-xl border-black h-screen">
-3
+<section class="flex flex-col w-[300px] border-xl border-black h-screen font-[Montserrat] ">
+    <img class="w-[100px] h-[36px] mt-8 mx-[100px] mb-[96px]" src="../assets/images/logo.png" alt="">
+    <div>
+        <a class="flex w-[236px] h-[48px] items-center" href="">
+            <img src="../assets/images/dashboard.svg" alt="">
+            <span class="text-base">Tổng quan</span>
+        </a>
+    </div>
+    
 </section>
