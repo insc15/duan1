b5589a0e678fd83887a4ec703c8c6c033a5d0339
Scr18 <nguyenquanghuyx8@gmail.com>
2022-11-29 21:16:36 +0700

    admin

--- views/partials/admin/header.php
+++ views/partials/admin/header.php
@@ -1,10 +1,30 @@
-<section class="flex flex-col w-[300px] border-xl border-black h-screen font-[Montserrat] ">
-    <img class="w-[100px] h-[36px] mt-8 mx-[100px] mb-[96px]" src="../assets/images/logo.png" alt="">
-    <div>
-        <a class="flex w-[236px] h-[48px] items-center" href="">
-            <img src="../assets/images/dashboard.svg" alt="">
-            <span class="text-base">Tổng quan</span>
+<section class="flex flex-col w-[300px] border-xl border-black h-screen font-[Montserrat] shadow-xl bg-[#F6F8FA]  ">
+        <img class="w-[100px] h-[36px] mt-8 mx-[100px] mb-[96px]" src="../assets/images/logo.png" alt="">
+        <a class="flex w-[236px] h-[48px] items-center mx-[32px] hover:shadow hover:drop-shadow-2xl hover:rounded-lg" href="">
+            <img class="ml-[20px]" src="../assets/images/dashboard.svg" alt="">
+            <span class="text-base ml-[15px]">Tổng quan</span>
         </a>
-    </div>
-    
+        <a class="flex w-[236px] h-[48px] items-center mx-[32px] hover:shadow hover:drop-shadow-2xl hover:rounded-lg" href="">
+            <img class="ml-[20px]" src="../assets/images/products.svg" alt="">
+            <span class="text-base ml-[15px]">Sản phẩm</span>
+        </a>
+        <a class="flex w-[236px] h-[48px] items-center mx-[32px] hover:shadow hover:drop-shadow-2xl hover:rounded-lg" href="">
+            <img class="ml-[20px]" src="../assets/images/cate.svg" alt="">
+            <span class="text-base ml-[15px]">Danh mục</span>
+        </a>
+        <a class="flex w-[236px] h-[48px] items-center mx-[32px] hover:shadow hover:drop-shadow-2xl hover:rounded-lg" href="">
+            <img class="ml-[20px]" src="../assets/images/order.svg" alt="">
+            <span class="text-base ml-[15px]">Đơn hàng</span>
+        </a>
+        <!-- profile -->
+        <a class="mx-[32px] mt-[583px] flex mb-[32px] hover:shadow hover:drop-shadow-2xl hover:rounded-lg w-[236px]">
+            <img class="w-[48px] h-[48px] rounded-lg" src="../assets/images/bgcart.jpg" alt="">
+            <div class="ml-[12px]">
+                <span class="text-base font-medium">Huy Quang Nguyen</span>
+                <span class="mt-[4px] text-xs font-light">huy_gay_2003@gmail.com</span>
+            </div>
+        </a>
+        <a href="" class="border border-red-700 mx-[32px] py-1 rounded text-center ">
+            <span class="text-red-700 ">Đăng xuất</span>
+        </a>
 </section>
