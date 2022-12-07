30789d8516c514cb4cc567d6e3bb361217a8689a
Scr18 <nguyenquanghuyx8@gmail.com>
2022-12-07 09:02:00 +0700

    up

--- views/pages/login.php
+++ views/pages/login.php
@@ -1,9 +1,9 @@
-<section class="bg-[url('../images/hero4.png')] h-screen">
-    <div class="w-[727px]">
+<section class="bg-[url('../images/hero4.png')] h-screen w-max-screen-xl">
+    <div class="w-4/12 mx-auto pt-20">
         <h1 class="text-xl">Đăng nhập</h1>
         <form action="" class="" method="post">
             <input name="username" type="text"
-                class="px-3 py-2 focus:shadow-default focus:border-gray-500 focus:outline-none placeholder:text-sm w-full border border-solid border-gray-300 rounded-sm my-2 placeholder:text-center"
+                class="px-3 py-2 focus:shadow-default focus:border-gray-500 focus:outline-none placeholder:text-sm w-full border border-solid border-gray-300 rounded-sm my-2 "
                 placeholder="Username">
             <input name="password" type="password"
                 class="px-3 py-2 focus:shadow-default focus:border-gray-500 focus:outline-none placeholder:text-sm w-full border border-solid border-gray-300 rounded-sm my-2"
@@ -15,7 +15,7 @@
             <?php if ($error_msg !== '') : ?>
             <p class="text-red-500"><?php echo $error_msg ?></p>
             <?php endif; ?>
-            <button class="w-full bg-primary text-primary py-2 px-3 mt-2" type="submit">Đăng nhập</button>
+            <button class="w-full bg-primary text-white py-2 px-3 mt-2" type="submit">Đăng nhập</button>
         </form>
         <p class="text-sm mt-2">Chưa có tài khoản? <a class="text-primary" href="/duan1/register">Đăng ký ngay</a></p>
     </div>
