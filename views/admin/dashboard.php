5d0ad7c025534fd2094a193c16501d8e46374970
viet <viet20000333@gmail.com>
2022-12-11 00:50:03 +0700

    11/12/2022

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -1,4 +1,55 @@
-<section class=" ml-5 py-9 font-[Montserrat]">
-    <h1 class="font-normal text-2xl md:text-4xl">Tổng Quan Website</h1>
-    <div class=""></div>
-</section>  
+<section class=" justify-items-start py-9 font-[Montserrat]">
+    <h1 class="font-normal text-2xl md:text-4xl ml-5 justify-center">Tổng Quan Website</h1>
+</section>
+<section>
+    <div class="flex flex-wrap">
+        <div class=" w-full flex pr-3">
+            <div class="grid gap-4 grid-cols-2  py-9  ">
+                
+                <div class="px-4 py-5 shadow-default rounded-lg flex  " style="width: 300px; height: 100px;">
+                    <img src="https://www.emojimeaning.com/img/img-apple-160/1f91d.png" class="" alt="">
+                    <h1 class="text-center py-2 font-[Montserrat] text-xl py-2"><a href="">
+                        Tổng Quan Sản Phẩm
+                    </a>
+                    <p class="text-orange-700">156</p>
+                </h1>
+                </div>
+                <div class="px-4 py-5 shadow-default rounded-lg flex  " style="width: 300px; height: 100px;">
+                    <img src="https://img.pikbest.com/png-images/qiantu/yellow-tick-pattern_2626970.png!bw700" class="" alt="">
+                    <h1 class="text-center py-2 font-[Montserrat] text-xl py-2"><a href="">
+                        Đơn Hàng Hoàn Thành
+                    </a>
+                    <p class="text-orange-700">30</p>
+                </h1>
+                </div>
+                <div class="px-4 py-5 shadow-default rounded-lg flex  " style="width: 300px; height: 100px;">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzH6TfTtq91hzmeIvm_4JOdb5y1UWjTlYZdA&usqp=CAU" class="" alt="">
+                    <h1 class="text-center py-2 font-[Montserrat] text-xl py-2"><a href="">
+                        Người Đùng mới
+                    </a>
+                    <p class="text-orange-700">3</p>
+                </h1>
+                </div>
+                <div class="px-4 py-5 shadow-default rounded-lg flex  " style="width: 300px; height: 100px;">
+                    <img src="https://cachlambanh.vn/wp-content/uploads/2017/10/price-icon.png" class="" alt="">
+                    <h1 class="text-center py-2 font-[Montserrat] text-xl py-2"><a href="">
+                        Doanh Thu
+                    </a>
+                    <p class="text-orange-700">10.000.00đ</p>
+                </h1>
+                </div>
+        </div>
+
+    </div>
+
+
+    <div class="w-full pl-3 flex  ">
+        
+        <div class=" text-center">Thống Kê Website Cara </div>
+        <canvas id="canvas" class=""></canvas>
+    </div>
+    <script>
+        
+    </script>
+
+</section>
